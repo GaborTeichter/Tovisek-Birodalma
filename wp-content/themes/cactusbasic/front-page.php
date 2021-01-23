fa6bb3c50a34950bc0256189eb6e57fa834f4f44
GaborTeichter <66086127+GaborTeichter@users.noreply.github.com>
2021-01-23 03:29:08 +0100

    modified pictures

--- wp-content/themes/cactusbasic/front-page.php
+++ wp-content/themes/cactusbasic/front-page.php
@@ -24,15 +24,15 @@
          <section class="page-section" id="rolunk">
             <div class="container">
                 <div class="text-center">
-                    <h2 class="section-heading text-uppercase">Rólunk</h2>
-                    <h3 class="section-subheading text-muted">Üdvözöllek az oldalamon.</h3>
+                    <h2 class="section-heading text-uppercase">Rólunk</h2><br>
+                    <!-- <h3 class="section-subheading text-muted">Üdvözöllek az oldalamon.</h3><br>-->
                 </div>
-                <div class="row ">
-                    <div class="col-md-4">
+                <div class="row">
+                    <div class="col-md-12">
                 
                         <blockquote class="blockquote text-muted">
-                        <p>Ha kedvező áron, nagy gonddal és szakértelemmel nevelt kaktuszokat keres, akkor ez az ön oldala! 
-                        Mindent megteszünk, hogy a legjobb termékekkel szolgálhassunk vevőink számára, kínálatunk folyamatosan bővül!
+                        <p>Ha kedvező áron, nagy gonddal és szakértelemmel nevelt kaktuszokat keres, akkor ez az ön oldala!  
+                        Mindent megteszünk, hogy a legjobb termékekkel szolgálhassunk vevőink számára, kínálatunk folyamatosan bővül! 
                         Forduljon hozzánk bizalommal!</p>
                         </blockquote>
                     </div>
@@ -44,7 +44,7 @@
 
         <!-- Termékek -->
         <section class="page-section bg-light">
-            <div class="container">
+            <div class="container-fluid">
                 <div class="text-center">
                     <h2 class="section-heading text-uppercase">Termékek</h2>
                     <h3 class="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
@@ -52,20 +52,20 @@
                
                 <div class="row">
                     <div class="card">
-                        <img src="..\assets\img\kaktusz-card.jpg" alt="kaktusz-card.jpg" style="width:100%">
+                        <img src="wp-content/themes/cactusbasic/assets/img/kaktusz-card.jpg" alt="kaktusz-card.jpg" style="width:100%">
                         <h1>Kaktuszok</h1>
                         <p>Tekintsd meg kaktuszaink széles választékát!</p>
                         <p><button>Megnézem</button></p>
                     </div>
 
                     <div class="card">
-                        <img src="img/pozsgas-card.jpg" alt="pozsgas-card.jpg" style="width:100%">
+                        <img src="wp-content/themes/cactusbasic/assets/img/pozsgas-card.jpg" alt="pozsgas-card.jpg" style="width:100%">
                         <h1>Pozsgások</h1>
                         <p>Tekintsd meg pozsgásaink széles választékát!</p>
                         <p><button>Megnézem</button></p>
                     </div>
-                </div>
-
+                </div> 
+        
             </div>
         </section>
         <!-- Termékek end -->
