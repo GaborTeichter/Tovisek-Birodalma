69177f0e2a4f4842274b42b922d3ea59a29e7720
GaborTeichter <66086127+GaborTeichter@users.noreply.github.com>
2021-01-28 20:24:17 +0100

    I fixed the footer2

--- wp-content/themes/cactusbasic/assets/js/main.js
+++ wp-content/themes/cactusbasic/assets/js/main.js
@@ -16,16 +16,6 @@ $(document).ready(function () {
     });
 });
 
-/*
-//Google Map API
-function myMap() {
-  var mapProp= {
-    center:new google.maps.LatLng(51.508742,-0.120850),
-    zoom:5,
-  };
-  var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
-  }
-*/
   
   //Coming soon
   
