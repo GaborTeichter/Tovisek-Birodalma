bd9c01ae861255c97b54f7f5d8a90315f7fafc3c
GaborTeichter <66086127+GaborTeichter@users.noreply.github.com>
2021-02-01 21:27:14 +0100

    I set up .htaccess file close #24

--- wp-content/themes/cactusbasic/front-page.php
+++ wp-content/themes/cactusbasic/front-page.php
@@ -92,7 +92,7 @@
          <!-- Kapcsolatok -->
         
             
-        <?php echo do_shortcode( '[contact-form-7 id="46" title="Kapcsolatok"]' ); ?>
+        <?php echo do_shortcode( '[contact-form-7 id="9" title="Kapcsolatok"]' ); ?>
 
        
         <!-- Kapsolatok end -->
